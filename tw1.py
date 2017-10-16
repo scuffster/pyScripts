72965fa31157f34015ebb28addc5b8c6d0123b96
scuffster <scuffster@gmail.com>
2017-10-16 22:47:27 +0000

    Update tw1.py

--- tw1.py
+++ tw1.py
@@ -2,10 +2,10 @@ import tweepy
  
 # Consumer keys and access tokens, used for OAuth
 # Your twitter API credentials
-consumer_key = 'VgyGLusD9rYL0yuctLbsYyv0L'
-consumer_secret = 'dxoPSmbF3ap9CPAnWSd6UePCSRiIiOczOEsC6XSgWsPehBf6F4'
-access_token = '15687368-uhUTmkQjrYTw8Vm7Hl9BxAxRJ6Mr5IZGgFYo1c1Ax'
-access_token_secret = 'BJw4WdGTurY6WjZ9qtFcNiZ4uYItva5Rz7G9KkxBurGsA'
+consumer_key = '<>replace this'
+consumer_secret = '<replace this>'
+access_token = '<replace his>'
+access_token_secret = '<replace this>'
  
 # OAuth process, using the keys and tokens
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
